e3dafd2be3c9f6e65dbbb2640c280272dfe0c180
marcelo-sl <mar.sousa2061@gmail.com>
2024-08-28 23:29:49 -0300

    feat: Creating projects.

---
+++ OneClickJobs.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OneClickJobs.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
