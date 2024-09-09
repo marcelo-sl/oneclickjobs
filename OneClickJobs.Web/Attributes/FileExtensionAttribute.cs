01ea357518a4aa015d8f5bf006e79068db060ff6
marcelo-sl <mar.sousa2061@gmail.com>
2024-09-09 07:06:08 -0300

    feat: Creating file extension attribute.

---
+++ OneClickJobs.Web/Attributes/FileExtensionAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OneClickJobs.Web.Attributes;
+
+public class FileExtensionAttribute : ValidationAttribute
+{
+    private readonly string[] _allowedExtensions;
+
+    public FileExtensionAttribute(string[] allowedExtensions)
+    {
+        _allowedExtensions = allowedExtensions;
+    }
+
+#pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+#pragma warning restore CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
+    {
+        if (value != null)
+        {
+            if (value is IFormFile file)
+            {
+                var extension = Path.GetExtension(file.FileName).ToLower();
+
+                if (!_allowedExtensions.Contains(extension))
+                {
+                    return new ValidationResult("Only files with extension " + string.Join(", ", _allowedExtensions) + " are allowed.");
+                }
+            }
+        }
+
+        return ValidationResult.Success!;
+    }
+}
--- OneClickJobs.Web/Data/ViewModels/Resumes/CreateResumeViewModel.cs
+++ OneClickJobs.Web/Data/ViewModels/Resumes/CreateResumeViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+using OneClickJobs.Web.Attributes;
+
 namespace OneClickJobs.Web.Data.ViewModels.Resumes;
 
 public class CreateResumeViewModel
 {
     [Required]
+    [FileExtensionAttribute([".pdf"])]
     public IFormFile FormFile { get; set; } = null!;
 }
