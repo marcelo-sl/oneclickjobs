645f250cff3dea885a7cfefd87e8a75d2c257037
marcelo-sl <mar.sousa2061@gmail.com>
2024-09-12 20:30:33 -0300

    feat: Creating categories pages.

---
+++ OneClickJobs.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using OneClickJobs.Domain.Entities;
+using OneClickJobs.Web.Data.Contexts;
+
+namespace OneClickJobs.Web.Controllers;
+
+[Authorize]
+public class CategoriesController(ApplicationDbContext context) : Controller
+{
+
+    // GET: Categories
+    public async Task<IActionResult> Index()
+    {
+        return View(await context.Categories.ToListAsync());
+    }
+
+    // GET: Categories/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var category = await context.Categories
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    // GET: Categories/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Categories/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Name,Id,CreatedAt,CreatedBy,UpdatedAt,UpdatedBy")] Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            context.Add(category);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+    }
+
+    // GET: Categories/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var category = await context.Categories.FindAsync(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
+
+    // POST: Categories/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Name,Id,CreatedAt,CreatedBy,UpdatedAt,UpdatedBy")] Category category)
+    {
+        if (id != category.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                context.Update(category);
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(category.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+    }
+
+    // GET: Categories/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var category = await context.Categories
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    // POST: Categories/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var category = await context.Categories.FindAsync(id);
+        if (category != null)
+        {
+            context.Categories.Remove(category);
+        }
+
+        await context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool CategoryExists(int id)
+    {
+        return context.Categories.Any(e => e.Id == id);
+    }
+}
